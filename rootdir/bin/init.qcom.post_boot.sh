99969e110158d1760b5ee8e4e7317f5da36aa0d2
YUREI <281564886+yureixen@users.noreply.github.com>
2026-08-30 00:49:18 +0600

    Revise ZRAM size configuration for better performance
    Updated ZRAM configuration to set size equal to physical RAM for devices with 2GB or more. Removed previous size calculations and added comments for clarity.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -588,39 +588,23 @@ KernelVersionB=${KernelVersionS%.*}
 function configure_zram_parameters() {
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
     MemTotal=${MemTotalStr:16:8}
-
     low_ram=`getprop ro.config.low_ram`
-
-    # Zram disk - 75% for Go devices.
-    # For 512MB Go device, size = 384MB, set same for Non-Go.
-    # For 1GB Go device, size = 768MB, set same for Non-Go.
-    # For 2GB Go device, size = 1536MB, set same for Non-Go.
-    # For >2GB Non-Go devices, size = 50% of RAM size. Limit the size to 4GB.
+    # Custom: ZRAM size = 1:1 with physical RAM (2GB->2GB, 3GB->3GB, 4GB->4GB).
     # And enable lz4 zram compression for Go targets.
-
     let RamSizeGB="( $MemTotal / 1048576 ) + 1"
     diskSizeUnit=M
-    if [ $RamSizeGB -le 2 ]; then
-        let zRamSizeMB="( $RamSizeGB * 1024 ) * 3 / 4"
-    else
-        let zRamSizeMB="( $RamSizeGB * 1024 ) / 2"
-    fi
-
-    # use MB avoid 32 bit overflow
+    let zRamSizeMB="( $RamSizeGB * 1024 )"
     if [ $zRamSizeMB -gt 4096 ]; then
         let zRamSizeMB=4096
     fi
-
     if [ "$low_ram" == "true" ]; then
         echo lz4 > /sys/block/zram0/comp_algorithm
     fi
-
     if [ -f /sys/block/zram0/disksize ]; then
         if [ -f /sys/block/zram0/use_dedup ]; then
             echo 1 > /sys/block/zram0/use_dedup
         fi
         echo "$zRamSizeMB""$diskSizeUnit" > /sys/block/zram0/disksize
-
         # ZRAM may use more memory than it saves if SLAB_STORE_USER
         # debug option is enabled.
         if [ -e /sys/kernel/slab/zs_handle ]; then
@@ -629,7 +613,6 @@ function configure_zram_parameters() {
         if [ -e /sys/kernel/slab/zspage ]; then
             echo 0 > /sys/kernel/slab/zspage/store_user
         fi
-
         mkswap /dev/block/zram0
         swapon /dev/block/zram0 -p 32758
     fi

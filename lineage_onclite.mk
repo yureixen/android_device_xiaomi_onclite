afd566219ef85bc1112abe2acb3c0d3af11a6d40
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-22 02:38:01 +0600

    onclite: Remove TARGET_BOOT_ANIMATION_RES assignment

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -14,7 +14,6 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 720
 TARGET_USES_MINI_GAPPS := true
 
 # Device identifier. This must come after all inclusions.

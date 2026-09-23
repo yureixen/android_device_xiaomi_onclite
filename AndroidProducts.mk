cff78bfbdd667fc602fd914d0b78a1cce0564f5e
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-24 00:48:16 +0600

    onclite: Update product makefile for derp_onclite

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/derp_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_onclite-user \
+    derp_onclite-userdebug \
+    derp_onclite-eng

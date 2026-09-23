c117fe26f1c74519f163719db91db61748e85268
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-24 03:08:45 +0600

    onclite: Remove call recording support from derp_onclite.mk
    Remove support for call recording in derp_onclite.

--- derp_onclite.mk
+++ derp_onclite.mk
@@ -15,7 +15,6 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 # Inherit some common DerpFest stuff.
 TARGET_USES_MINI_GAPPS := true
 TARGET_NOT_USES_BLUR := true
-TARGET_SUPPORTS_CALL_RECORDING := true
 $(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.

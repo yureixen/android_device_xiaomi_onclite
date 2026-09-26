5956e6a7764912d3f5fb79ed5a02af381587abfe
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-26 23:03:51 +0600

    onclite: Add key inheritance for lineage_onclite

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_onclite

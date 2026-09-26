c17203778a24acf6b6819282cb41b7df72cb0fff
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-26 22:59:19 +0600

    onclite: Add keys.mk inheritance to lineage_onclite.mk

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 TARGET_USES_MINI_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
